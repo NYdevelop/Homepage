4592a5608bb056ebb16bacfa0ccf79941dc66e32
towa252 <gt.srai@gmail.com>
2022-08-01 01:36:32 +0900

    navigationクリック時の移動調整

--- js/script.js
+++ js/script.js
@@ -1,38 +1,36 @@
 $(function(){
-	
-	var
-	  winW = $(window).width(),
-		winH = $(window).height(),
-		nav = $('#mainnav ul a'),
-		curPos = $(this).scrollTop();
-	
-	if (winW > 800){
-		var headerH =0;
-	}
-	else{
-		var headerH =70;
-	}
-	
-	$(nav).on('click', function(){
-  	var $el = $(this),
-		id = $el.attr('href');
- 		$('html, body').animate({
-   		scrollTop: $(id).offset().top - headerH
- 		}, 500);
-		if (winW < 890){
-			$('#menuWrap').next().slideToggle();
-			$('#menuBtn').removeClass('close');
-		}
- 		return false;
-	});
-	
-	$('.panel').hide();
-	$('#menuWrap').toggle(function(){
-		$(this).next().slideToggle();
-		$('#menuBtn').toggleClass('close');
-	},
-	function(){
-		$(this).next().slideToggle();
-		$('#menuBtn').removeClass('close');
-	});
+  $('#mainnav ul a').on('click', function(){
+    var winW = $(window).width();
+    if (winW > 800){
+      var headerH =0;
+    }
+    else{
+      var headerH =70;
+    }
+    
+    var $el = $(this),
+        id = $el.attr('href');
+     $('html, body').animate({
+       scrollTop: $(id).offset().top - headerH
+     }, 500);
+    if (winW < 890){
+      $('#menuWrap').next().slideToggle();
+      $('#menuBtn').removeClass('close');
+    }
+     return false;
+  });
+  
+  $('.panel').hide();
+  $('#menu').on('click', function(){
+    var $el = $('#menuBtn');
+    if ($el.hasClass('close'))
+    {
+      $el.removeClass('close');
+    }
+    else
+    {
+      $el.toggleClass('close');
+    }
+    $('#menuWrap').next().slideToggle();
+  });
 });
